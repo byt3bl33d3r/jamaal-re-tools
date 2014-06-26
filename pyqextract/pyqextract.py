acee6c99ade5ee35505840ee4ca556133dcf2c31
ff <jamaal.speights@gmail.com>
2014-06-26 04:31:30 -0400

    updated pyqextract.py

--- pyqextract/pyqextract.py
+++ pyqextract/pyqextract.py
@@ -1,12 +1,12 @@
-# pyqextract.py 
+# jsqextract.py 
 # Symantec quarantine file decoder
 # jamaal.speights@gmail.com
 # Symantec xor's quarantine executables with 0xA5, while the symantec header is xor'ed with
 # 0x5a.  Symantec also adds ODB (OL' DIRTY BYTES) to the executable offsetting the IAT,
 # code and data segments.
-# pyqextract.py fixes all this
+# jsqextract.py fixes all this
 # uncomment out the print header statement to view the header
-# v0.1 
+# version 0.2 
 
 import sys
 import os
@@ -20,36 +20,33 @@ def main(argv):
         try:
             fh = argv[1]
             outh = argv[2]
-            dheader ="5A5A5A5A5A5A5A5A72"
-            dfooter="A6A5A5A5A7A5A5A5"  
-            mzheader = "4D5A900003000000"
-            odbA= "F6FFEFFFFF"  #ol dirty bytes
-            odbB = "F6ABFFFFFF" 
+            dheader = unhexlify("5A5A5A5A5A5A5A5A72")
+            dfooter= unhexlify("A6A5A5A5A7A5A5A5")
+            mzheader = unhexlify("4D5A900003000000")
+            odbA= unhexlify("F6FFEFFFFF")  # // Ol Dirty Bytes
+            odbB = unhexlify("F6ABFFFFFF") # // Ol Dirty Bytes
             header = "" 
-            binstr = ""
-            infile = open(fh,'rb').read()
-            infile = hexlify(infile).upper()
-            header = infile[infile.find(dheader):(infile.find(dfooter))]
-            footer = infile[(infile.find(dfooter)):]
+            binstr = header
             
-            for i in xrange(0, len(header), 2):
-                header += "%02x" % (0x5a ^ int(header[i:i+2],0x10))
-            #print unhexlify(header)
-                
-            for i in xrange(0, len(footer), 2):
-                binstr += "%02x" % (0xA5 ^ int(footer[i:i+2],0x10))
+            infile = open(fh,'rb').read()
+            header = infile[infile.find(dheader):infile.find(dfooter)]
+            footer = infile[(infile.find(dfooter)):]
 
-            header = header.upper()
-            binstr = binstr.upper()
+            for i in range(len(header)):
+                header += chr(0x5a ^ ord(header[i]))
+
+            for i in range(len(footer)):
+                binstr += chr(0xA5 ^ ord(footer[i]))
+
             binstr =  binstr[binstr.find(mzheader):]
             binstr = binstr.replace(odbA,"")
             binstr = binstr.replace(odbB,"")
-            binstr = unhexlify(binstr)
             if binstr:
                 f = open(outh,'wb')
                 f.write(binstr)
                 f.close()
                 print "done..."
+                
         except Exception,e:
             print "failed: ",str(e)
 
